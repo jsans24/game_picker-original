5e01fb87419721e07188ffa6b79d4e9d3f4393df
jsans24 <jsans24@gmail.com>
2020-12-03 10:55:41 -0800

    commit 14 - removed info from profile page
    Signed-off-by: jsans24 <jsans24@gmail.com>

--- game_picker/src/Views/profile.js
+++ game_picker/src/Views/profile.js
@@ -24,11 +24,6 @@ const Profile = () => {
           <p className="lead text-muted">{user.email}</p>
         </div>
       </div>
-      <div className="row">
-        <pre className="col-12 text-light bg-dark p-4">
-          {JSON.stringify(user, null, 2)}
-        </pre>
-      </div>
     </div>
   );
 };
